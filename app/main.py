37c1d4aa7fac852494d12a29e3d06261ee8f61fa
sidd Gupta <sidd.gupta@gmail.com>
2021-08-23 13:14:58 +0530

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from fastapi import FastAPI, Depends
+
+from app.config import get_settings, Settings
+
+app = FastAPI()
+
+
+@app.get("/ping")
+async def pong(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong!",
+        "environment": settings.environment,
+        "testing": settings.testing
+        }
