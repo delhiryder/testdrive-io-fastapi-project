54a3acbded1e930ebb27172cae79403f6072ef23
sidd Gupta <sidd.gupta@gmail.com>
2021-08-31 11:41:22 +0530

    Recovered .git file from remote. Lesson learned (*must* push daily)

---
+++ project/app/main.py
@@ -0,0 +1,34 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import logging
+
+from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+
+from app.api import ping, summaries
+from app.db import init_db
+
+log = logging.getLogger("uvicorn")
+
+def create_application() -> FastAPI:
+    application = FastAPI()
+    application.include_router(ping.router)
+    application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])  # new
+    return application
+
+
+app = create_application()
+
+
+@app.on_event("startup")
+async def startup_event():
+    log.info("Starting up...")
+    init_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down...")
